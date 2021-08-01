7dc1fa7244c85bf72b14990afd72188947e20d9f
Daisy Collins <daisydcollins12@gmail.com>
2021-07-31 21:35:31 -0700

    create lifeExpect function

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,35 @@
 export default class Age {
-  constructor (age,lifeExpect) {
-    this.age = age;
-    this.lifeExpect = lifeExpect;
+  constructor (userAge,userLifeExpect) {
+    this.userAge = userAge;
+    this.userLifeExpect = userLifeExpect;
+  }
+  planetAge(planet) {
+    if (planet === "Murcury") {
+      return Math.floor(this.userAge / .24);
+    } 
+    else if (planet === "Venus") {
+      return Math.floor(this.userAge  / .62);
+    } 
+    else if (planet === "Mars") {
+      return Math.floor(this.userAge  / 1.88);
+    } 
+    else if (planet === "Jupiter") {
+      return Math.floor(this.userAge / 11.86);
+    }
+  }
+
+  lifeExpect(planet) {
+    if(planet === "Murcury") {
+      return Math.floor(this.userLifeExpect / .24);
+    } 
+    else if (planet === "Venus") {
+      return Math.floor(this.userLifeExpect / .62);
+    } 
+    else if (planet === "Mars") {
+      return Math.floor(this.userLifeExpect/ 1.88);
+    } 
+    else if (planet === "Jupiter") {
+      return Math.floor(this.userLifeExpect / 11.86);
+    }
   }
 }
